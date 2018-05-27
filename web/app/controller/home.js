3450465f12d2d637435794954e2810fbd0cd2362
tangxiaopeng <630787976@qq.com>
2018-05-27 16:35:06 +0800

    发布

--- web/app/controller/home.js
+++ web/app/controller/home.js
@@ -4,7 +4,17 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = this.ctx.req.url
+    const {ctx} = this
+    // this.ctx.body = JSON.stringify(this.ctx.req.url)//this.ctx.req.url
+    const result = await ctx.service.app.curl()
+    // console.log({result})
+    if(!result){
+      ctx.status = 404
+      return ctx.body = 'Can not find Serive'
+    }
+    ctx.status = result.status;
+    ctx.set(result.headers);
+    ctx.body = result.data;
   }
 }
 
--- web/app/service/app.js
+++ web/app/service/app.js
@@ -5,21 +5,23 @@
 
 const Service = require('egg').Service
 
+const Url = require('url')
+
 module.exports = class AppService extends Service {
 
     async queryAllowHost(){
-        let list = this.ctx.service.service.getCanUseServiceList()
-        if(list.length === 0){
+        let list = await this.ctx.service.service.getCanUseServiceList()
+        if(!list){
             return
         }
         const url = this.ctx.req.url
-        return list.filter(info => {
+        return (Array.isArray(list) && list || [list]).filter(info => {
             const r = new RegExp(info.rule)
             return r.test(url)
         }).sort((next, pre) => next.level - pre.level).pop()
     }
     /**
-     * 
+     *  
      * @param requestCtrl 用户请求
      */
     async curl(){
@@ -27,5 +29,19 @@ module.exports = class AppService extends Service {
         if(!host){
             return 
         }
+        const url = Url.format({
+            host: [host.host, host.port].join(':'),
+            protocol: host.protocol || 'http:',
+            pathname: this.ctx.req.url,
+        })
+        const result = await new Promise(resolve => this.ctx.curl(url, {
+            method: this.ctx.req.method,
+            headers: this.ctx.req.headers,
+            data: this.ctx.req.method === 'post' ? this.ctx.req.body : null
+        }).then(resolve).catch(e => {
+            this.app.logger.info(e)
+            resolve()
+        })) 
+        return result
     }
 }
